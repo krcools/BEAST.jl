21801baafe0cbcfd09e5bc4d883f63021b0f07d2
Alexandre Dély <eexad9@nottingham.ac.uk>
2017-11-08 13:23:07 +0000

    Cleaned the TDEFIE IRK CQ example

--- examples/tdefie_irk.jl
+++ examples/tdefie_irk.jl
@@ -1,10 +1,8 @@
-include("../src/BEAST.jl")
-using CompScienceMeshes, BEAST, StaticArrays
+using CompScienceMeshes, BEAST
 
 o, x, y, z = euclidianbasis(3)
 
 sol = 5.0;
-#Δt, Nt = 0.08/sol,400
 Δt, Nt = 100.0/sol,200
 
 D, Δx = 1.0, 0.45
@@ -21,12 +19,11 @@ direction, polarisation = z, x
 E = planewave(polarisation, direction, derive(gaussian), sol)
 
 LaplaceEFIO{T}(s::T) = MWSingleLayer3D(-s/sol, s*s/sol, T(sol));
-T = RungeKuttaConvolutionQuadrature(LaplaceEFIO, A, b, Δt, 15, 1.0001);
+kmax = 15;
+rho = 1.0001;
+T = RungeKuttaConvolutionQuadrature(LaplaceEFIO, A, b, Δt, kmax, rho);
 
 @hilbertspace j
 @hilbertspace j′
 tdefie = @discretise T[j′,j] == -1E[j′]   j∈V  j′∈V
 xefie_irk = solve(tdefie)
-
-using PyPlot
-semilogy(abs.(xefie_irk[2,:]))
