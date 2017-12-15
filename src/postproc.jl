8b737565c4115c7e1685b5d6e4375ec4c63cdeb9
Shakirudeen Lasisi <Shakirudeen.Lasisi@nottingham.ac.uk>
2017-12-15 10:39:10 +0000

    added tdfarfield and tdfacecurrents methods

--- src/postproc.jl
+++ src/postproc.jl
@@ -42,6 +42,56 @@ function facecurrents(coeffs, basis)
 	return fcr, geometry(basis)
 end
 
+function facecurrents(coeffs, basis::SpaceTimeBasis)
+
+	space_basis = basis.space
+	time_basis = basis.time
+
+	Nt = numfunctions(time_basis)
+	Δt = timestep(time_basis)
+
+	refs = refspace(space_basis)
+	trefs = refspace(time_basis)
+	numrefs = numfunctions(refs)
+	tnumrefs = numfunctions(trefs)
+
+	cells, ad = assemblydata(space_basis)
+	tcells, tad = assemblydata(time_basis)
+
+	mesh = geometry(space_basis)
+	T = eltype(coeffs)
+	D = dimension(mesh)
+	U = D+1
+
+	# TODO: express relative to input types
+	PT = SVector{U, T}
+	fcr = zeros(PT, numcells(mesh), Nt)
+
+	for (k,tcell) in enumerate(tcells)
+		tmps = neighborhood(tcell,1)
+		tvals = trefs(tmps)
+		for (p,cell) in enumerate(cells)
+			mps = center(cell)
+	        vals = refs(mps)
+
+			# assemble
+			for i in 1:numrefs
+				fx = vals[i][1]
+				for j in 1:tnumrefs
+					tfx = tvals[j]
+	    			for (m,a) in ad[p,i]
+						for (n,b) in tad[k,j]
+							fcr[p,k] += (coeffs[m,n] * tfx * b) * a * fx
+						end
+					end
+				end
+			end
+
+		end
+	end
+
+	return fcr, geometry(space_basis)
+end
 
 function facecurrents(u, X1, Xs...)
 
@@ -79,6 +129,14 @@ function potential(op, points, coeffs, basis)
 	T = SVector{3,Complex128}
 	ff = zeros(T, length(points))
 	store(v,m,n) = (ff[m] += v*coeffs[n])
+	potential!(store, op, points, basis)
+	return ff
+end
+
+function BEAST.potential(op, points,coeffs, basis::SpaceTimeBasis)
+	T = SVector{3,Complex128}
+	ff = zeros(T, length(points), size(coeffs)[2])
+	store(v,m,n,k,o) = (ff[m,k] += v*coeffs[n,o])
 	potential!(store, op, points, basis)
 	return ff
 end
@@ -143,6 +201,66 @@ function potential!(store, op, points, basis)
 
 end
 
+function BEAST.potential!(store, op, points, basis::SpaceTimeBasis)
+
+    space_basis = basis.space
+	tb = time_basis = basis.time
+
+	Nt = numfunctions(time_basis)
+	Δt = timestep(time_basis)
+
+	T = SVector{3,Complex128}
+
+	refs = refspace(space_basis)
+	trefs = refspace(time_basis)
+
+	numrefs = numfunctions(refs)
+	tnumrefs = numfunctions(trefs)
+
+	els, ad = assemblydata(space_basis)
+	tels, tad = assemblydata(time_basis)
+
+
+	zlocal = Array{T}(numrefs,Nt)
+	qdata = quaddata(op,refs,trefs,els,tels)
+
+	#println("Computing nearfield.")
+	print("dots out of 10: ")
+
+	todo, done, pctg = Nt, 0, 0
+    for (k,tel) in enumerate(tels)
+    	for (p,y) in enumerate(points)
+    		for (q,el) in enumerate(els)
+
+    			fill!(zlocal,zero(T))
+    			qr = quadrule(op,refs,trefs,p,y,q,el,k,tel,qdata)
+	    		farfieldlocal!(zlocal,op,refs,trefs,p,y,q,el,k,tel,tb,qr)
+
+    			# assemble from local contributions
+    			 for r in 1:numrefs
+					 for s in 1:Nt
+	                    for (n,b) in ad[q,r]
+	        				store(zlocal[r,s]*b,p,n,k,s)
+							# end
+	    				end
+					end
+    			end
+            end
+    	end
+
+		done += 1
+		new_pctg = round(Int, done / todo * 100)
+		if new_pctg > pctg + 9
+				#println(todo," ",done," ",new_pctg)
+				print(".")
+				pctg = new_pctg
+		end
+    end
+
+	println("")
+
+end
+
 function farfieldlocal!(zlocal,op,refspace,y,el,qr)
 
     for q in qr
@@ -157,4 +275,27 @@ function farfieldlocal!(zlocal,op,refspace,y,el,qr)
 
     end
 
+end
+
+function BEAST.farfieldlocal!(zlocal,op,trialrefs, timerefs,
+        p, testel, q, trialel, k, timeel, tb,quadrule)
+
+	M,N = size(zlocal)
+	Δt = tb.timestep
+
+    for qr in quadrule[1]
+        x = qr.point
+        F = qr.value
+        dx = qr.weight
+
+        krn = kernelvals(op, testel, x)
+
+        for r = 1:M
+			for l = 1:N
+				t = (k - l)*Δt
+            	zlocal[r,l] += integrand(op,krn,testel,F[r], t, tb) * dx
+			end
+		end
+
+    end
 end
