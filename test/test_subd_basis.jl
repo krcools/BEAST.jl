1903b8932cfa52be91e555f9e9fa26cb1194bee0
Kristof Cools <krcools@gmail.com>
2018-01-27 17:24:53 +0000

    Subdivision test runs

--- test/test_subd_basis.jl
+++ test/test_subd_basis.jl
@@ -1,15 +1,15 @@
 using CompScienceMeshes,BEAST
-# G = joinpath(dirname(@__FILE__),"sphere.in")
+G = readmesh(joinpath(dirname(@__FILE__),"..","examples","sphere872.in"))
  # G0 = readmesh(fn)
 # G0 = meshsphere(1.0, 0.5)
-G = readmesh("/Users/Benjamin/Documents/sphere.in")
+#G = readmesh("/Users/Benjamin/Documents/sphere.in")
 # G = Loop_subdivision(G0)
 X = subdsurface(G)
 
 els, ad = BEAST.assemblydata(X)
 
-# identityop    = Identity()
-singlelayer = BEAST.HH3DSingleLayerFDBIO(1.0)
-# I = assemble(identityop, X, X)
-S = assemble(singlelayer, X, X)
+identityop    = Identity()
+#singlelayer = BEAST.HH3DSingleLayerFDBIO(1.0)
+I = assemble(identityop, X, X)
+#S = assemble(singlelayer, X, X)
 # ncd = cond(I)
