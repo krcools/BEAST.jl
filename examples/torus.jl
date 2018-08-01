4eb4ff2525303bcb57ed7c2af063629ffbd71d54
Kristof Cools <krcools@gmail.com>
2018-08-01 16:08:03 +0200

    MFIE is now also using SSQ

--- examples/torus.jl
+++ examples/torus.jl
@@ -1,39 +1,5 @@
 using CompScienceMeshes, BEAST, BogaertInts10
 
-# function BEAST.quadrule(op::BEAST.MaxwellOperator3D, g::BEAST.RTRefSpace, f::BEAST.RTRefSpace, i, τ, j, σ, qd)
-#
-#     dtol = 1.0e3 * eps(eltype(eltype(τ.vertices)))
-#     xtol = 0.2
-#     k = norm(op.gamma)
-#     hits = 0
-#     xmin = xtol
-#     for t in τ.vertices
-#         for s in σ.vertices
-#             d = norm(t-s)
-#             xmin = min(xmin, k*d)
-#             if d < dtol
-#                 hits +=1
-#                 break
-#             end
-#         end
-#     end
-#
-#     hits == 3   && return BogaertInts10.BogaertSelfPatchStrategy(5)
-#     hits == 2   && return BogaertInts10.BogaertEdgePatchStrategy(8, 4)
-#     hits == 1   && return BogaertInts10.BogaertPointPatchStrategy(2, 3)
-#     xmin < xtol && return BEAST.WiltonSEStrategy(
-#         qd.tpoints[1,i],
-#         BEAST.DoubleQuadStrategy(
-#             qd.tpoints[2,i],
-#             qd.bpoints[2,j], #         ),
-#     )
-#     return BEAST.DoubleQuadStrategy(
-#         qd.tpoints[1,i],
-#         qd.bpoints[1,j],
-#     )
-# end
-
-
 fn = joinpath(dirname(@__FILE__),"assets","torus.msh")
 m = CompScienceMeshes.read_gmsh_mesh(fn)
 
--- src/maxwell/sauterschwabints.jl
+++ src/maxwell/sauterschwabints.jl
@@ -144,6 +144,59 @@ function momintegrals!(op::MWSingleLayer3D,
 end
 
 
+struct MWDL3DIntegrand{C,O,L}
+        test_triangular_element::C
+        trial_triangular_element::C
+        op::O
+        test_local_space::L
+        trial_local_space::L
+end
+
+function (igd::MWDL3DIntegrand)(u,v)
+
+        γ = igd.op.gamma
+
+        x = neighborhood(igd.test_triangular_element,u)
+        y = neighborhood(igd.trial_triangular_element,v)
+
+        r = cartesian(x) - cartesian(y)
+        R = norm(r)
+        G = exp(-γ*R)/(4π*R)
+
+        GG = -(γ + 1/R) * G / R * r
+        T = @SMatrix [
+            0 -GG[3] GG[2]
+            GG[3] 0 -GG[1]
+            -GG[2] GG[1] 0 ]
+
+        f = igd.test_local_space(x)
+        g = igd.trial_local_space(y)
+
+        jx = jacobian(x)
+        jy = jacobian(y)
+        j = jx*jy
+
+        G1 = j*T*g[1][1]
+        G2 = j*T*g[2][1]
+        G3 = j*T*g[3][1]
+
+        f1 = f[1][1]
+        f2 = f[2][1]
+        f3 = f[3][1]
+
+        SMatrix{3,3}(
+            dot(f1,G1),
+            dot(f2,G1),
+            dot(f3,G1),
+            dot(f1,G2),
+            dot(f2,G2),
+            dot(f3,G2),
+            dot(f1,G3),
+            dot(f2,G3),
+            dot(f3,G3),)
+end
+
+
 function momintegrals!(op::MWDoubleLayer3D,
     test_local_space::RTRefSpace, trial_local_space::RTRefSpace,
     test_triangular_element, trial_triangular_element, out, strat::SauterSchwabStrategy)
@@ -152,53 +205,62 @@ function momintegrals!(op::MWDoubleLayer3D,
         test_triangular_element.vertices,
         trial_triangular_element.vertices, strat)
 
-    test_triangular_element  = simplex(test_triangular_element.vertices[I]...)
-    trial_triangular_element = simplex(trial_triangular_element.vertices[J]...)
+    test_triangular_element  = simplex(
+        test_triangular_element.vertices[I[1]],
+        test_triangular_element.vertices[I[2]],
+        test_triangular_element.vertices[I[3]])
 
-    γ = op.gamma
+    trial_triangular_element = simplex(
+        trial_triangular_element.vertices[J[1]],
+        trial_triangular_element.vertices[J[2]],
+        trial_triangular_element.vertices[J[3]])
 
-    function igd(u,v)
+    # γ = op.gamma
+    #
+    # function igd(u,v)
+    #
+    #     x = neighborhood(test_triangular_element,u)
+    #     y = neighborhood(trial_triangular_element,v)
+    #
+    #     r = cartesian(x) - cartesian(y)
+    #     R = norm(r)
+    #
+    #     expn = exp(-γ*R)
+    #     G = expn / (4π*R)
+    #     GG = -(γ + 1/R) * G / R * r
+    #     T = @SMatrix [
+    #         0 -GG[3] GG[2]
+    #         GG[3] 0 -GG[1]
+    #         -GG[2] GG[1] 0 ]
+    #
+    #     f = test_local_space(x)
+    #     g = trial_local_space(y)
+    #
+    #     jx = jacobian(x)
+    #     jy = jacobian(y)
+    #
+    #     T1 = T*g[1][1]
+    #     T2 = T*g[2][1]
+    #     T3 = T*g[3][1]
+    #
+    #     f1 = f[1][1]
+    #     f2 = f[2][1]
+    #     f3 = f[3][1]
+    #
+    #     jx*jy*SMatrix{3,3}(
+    #         dot(f1, T1),
+    #         dot(f2, T1),
+    #         dot(f3, T1),
+    #         dot(f1, T2),
+    #         dot(f2, T2),
+    #         dot(f3, T2),
+    #         dot(f1, T3),
+    #         dot(f2, T3),
+    #         dot(f3, T3),);
+    # end
 
-        x = neighborhood(test_triangular_element,u)
-        y = neighborhood(trial_triangular_element,v)
-
-        r = cartesian(x) - cartesian(y)
-        R = norm(r)
-
-        expn = exp(-γ*R)
-        G = expn / (4π*R)
-        GG = -(γ + 1/R) * G / R * r
-        T = @SMatrix [
-            0 -GG[3] GG[2]
-            GG[3] 0 -GG[1]
-            -GG[2] GG[1] 0 ]
-
-        f = test_local_space(x)
-        g = trial_local_space(y)
-
-        jx = jacobian(x)
-        jy = jacobian(y)
-
-        T1 = T*g[1][1]
-        T2 = T*g[2][1]
-        T3 = T*g[3][1]
-
-        f1 = f[1][1]
-        f2 = f[2][1]
-        f3 = f[3][1]
-
-        jx*jy*SMatrix{3,3}(
-            dot(f1, T1),
-            dot(f2, T1),
-            dot(f3, T1),
-            dot(f1, T2),
-            dot(f2, T2),
-            dot(f3, T2),
-            dot(f1, T3),
-            dot(f2, T3),
-            dot(f3, T3),);
-    end
-
+    igd = MWDL3DIntegrand(test_triangular_element, trial_triangular_element,
+        op, test_local_space, trial_local_space)
     Q = sauterschwab_parameterized(igd, strat)
     for j ∈ 1:3
         for i ∈ 1:3
