e54bec61eb966a60948d00d8075213a7d735b3e5
Kristof Cools <krcools@gmail.com>
2020-02-05 16:10:04 +0100

    ex_fem charge density comp using dual space

--- examples/ex_fem.jl
+++ examples/ex_fem.jl
@@ -42,6 +42,42 @@ function isdivconforming(space)
     return TotalFlux, I
 end
 
+
+import Plotly
+function showfn(space,i)
+    geo = geometry(space)
+    T = coordtype(geo)
+    X = Dict{Int,T}()
+    Y = Dict{Int,T}()
+    Z = Dict{Int,T}()
+    U = Dict{Int,T}()
+    V = Dict{Int,T}()
+    W = Dict{Int,T}()
+    for sh in space.fns[i]
+        chrt = chart(geo, cells(geo)[sh.cellid])
+        nbd = CompScienceMeshes.center(chrt)
+        vals = refspace(space)(nbd)
+        x,y,z = cartesian(nbd)
+        # @show vals[sh.refid].value
+        u,v,w = vals[sh.refid].value
+        # @show x, y, z
+        # @show u, v, w
+        X[sh.cellid] = x
+        Y[sh.cellid] = y
+        Z[sh.cellid] = z
+        U[sh.cellid] = get(U,sh.cellid,zero(T)) + sh.coeff * u
+        V[sh.cellid] = get(V,sh.cellid,zero(T)) + sh.coeff * v
+        W[sh.cellid] = get(W,sh.cellid,zero(T)) + sh.coeff * w
+    end
+    X = collect(values(X))
+    Y = collect(values(Y))
+    Z = collect(values(Z))
+    U = collect(values(U))
+    V = collect(values(V))
+    W = collect(values(W))
+    Plotly.cone(x=X,y=Y,z=Z,u=U,v=V,w=W)
+end
+
 tetrs = CompScienceMeshes.tetmeshsphere(1.0, 0.15)
 @show numcells(tetrs)
 
@@ -135,6 +171,8 @@ QcurlX = assemble(Id, Q, curlX)
 v = QXplus \ (QcurlX * u)
 fcr1, geo1 = facecurrents(v, BEAST.ttrace(Xplus, bnd_tetrs));
 fcr2, geo2 = facecurrents(u, BEAST.ttrace(curl(X), bnd_tetrs));
+fcr3, geo3 = facecurrents(v, divergence(ttXplus));
+
 
 # tetrs1 = skeleton(tetrs,1)
 # tetrs2 = skeleton(tetrs,2)
