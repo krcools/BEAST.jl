6cd1d55e121c9aab901c61f7d6c8c3c63fa58cf9
RodVoskamp <R.Voskamp@student.tudelft.nl>
2020-03-04 11:38:28 +0100

    returna

--- src/maxwell/nearfield.jl
+++ src/maxwell/nearfield.jl
@@ -39,43 +39,5 @@ function integrand(op::MWSingleLayerField3D, krn, y, fp, p)
     G = krn.green
     ∇G = krn.gradgreen
 
-    #-γ*G*j + ∇G.*ρ/γ
-    -γ*G*j + ∇G.*ρ/γ
-end
-
-mutable struct MWDoubleLayerField3D{K}
-    wavenumber::K
-end
-
-"""
-    MWDoubleLayerField3D(wavenumber = error())
-
-Create the double layer near field operator, for use with `potential`.
-"""
-MWDoubleLayerField3D(;wavenumber = error("Missing arg: `wavenumber`")) = MWDoubleLayerField3D(wavenumber)
-
-
-quaddata(op::MWDoubleLayerField3D,rs,els) = quadpoints(rs,els,(2,))
-quadrule(op::MWDoubleLayerField3D,refspace,p,y,q,el,qdata) = qdata[1,q]
-
-function kernelvals(op::MWDoubleLayerField3D,y,p)
-
-        k = op.wavenumber
-        r = y - cartesian(p)
-        R = norm(r)
-
-        kr = k * R
-        expn = exp(-im * k * R)
-        green = expn / (4pi*R)
-        gradgreen = -(im*k + 1/R) * green / R * r
-
-        KernelValsMaxwell3D(im*k, r, R, green, gradgreen)
-end
-
-function integrand(op::MWDoubleLayerField3D, krn, y, fp, p)
-
-    j = fp[1]
-    ∇G = krn.gradgreen
-
-    j×∇G
+    -γ*G*j + ∇G*ρ/γ
 end
