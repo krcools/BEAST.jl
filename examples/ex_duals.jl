6fd6fa98f7942544d4db2abe32c3be821c9ac35f
Kristof Cools <krcools@gmail.com>
2020-03-29 15:19:49 +0200

    Build CN step matrix and check eigvals

--- examples/ex_duals.jl
+++ examples/ex_duals.jl
@@ -44,10 +44,36 @@ idx = findfirst(abs.(EV.values) .> 1e-6)
 u = real(EV.vectors[:,idx+1])
 
 hemi = submesh(Tetrs) do Tetr
-    cartesian(center(chart(Tetrs, Tetr)))[3] < 0
+    cartesian(CompScienceMeshes.center(chart(Tetrs, Tetr)))[3] < 0
 end
 
 primal1_hemi = restrict(primal1, hemi)
 trace_primal1_hemi = BEAST.ttrace(primal1_hemi, boundary(hemi))
 fcr, geo = facecurrents(u, trace_primal1_hemi)
 Plotly.plot(patch(geo, norm.(fcr)))
+
+
+tetrs, bnd, dir, v2t, v2n = BEAST.dual1forms_init(Tetrs, Dir)
+duals11 = BEAST.dual1forms_body(int_Faces[collect(1:10)], tetrs, bnd, dir, v2t, v2n)
+
+Ggf = assemble(Id, dual1, primal2);
+Cgg = assemble(Id, dual1, curl(primal1))
+
+
+Cfg = Ggf \ Cgg;
+Conn = Matrix(connectivity(int_Edges, int_Faces))
+@show norm(Cfg - Conn)
+
+Zpp = zeros(numfunctions(primal1), numfunctions(primal1))
+Zdd = zeros(numfunctions(dual1), numfunctions(dual1))
+
+Ipp = assemble(Id, primal1, primal1)
+Idd = assemble(Id, dual1, dual1)
+
+Zpd = zeros(numfunctions(primal1), numfunctions(dual1))
+
+C = [Zpp Cgg'; -Cgg Zdd];
+J = [Ipp Zpd; Zpd' Idd];
+
+P = inv(2J - C) * (2J + C);
+Q = (2J - C) * inv(2J + C);
