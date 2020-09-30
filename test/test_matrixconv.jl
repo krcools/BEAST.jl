deac1c28c8a3be50c64d6f71f2d7bfa5940de178
Kristof Cools <krcools@gmail.com>
2020-09-30 21:22:40 +0200

    fixed broken MatrixConv test (the test itself was broken)

--- test/test_matrixconv.jl
+++ test/test_matrixconv.jl
@@ -7,6 +7,6 @@ A21 = BEAST.MatrixConvolution(rand(4,3,8))
 A12 = BEAST.MatrixConvolution(rand(2,5,12))
 A22 = BEAST.MatrixConvolution(rand(4,5,14))
 
-A = BEAST.hvcat((2,2), A11, A21, A12, A22)
+A = BEAST.hvcat((2,2), A11, A12, A21, A22)
 
 @test A[3,5,6] == A22[1,2,6]
