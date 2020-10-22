f422d628a54b097bb3eecb9c9ec8df49597a4732
Kristof Cools <krcools@gmail.com>
2020-10-22 10:55:53 +0200

    Modified boundary conditions in ex_duals

--- examples/ex_duals.jl
+++ examples/ex_duals.jl
@@ -5,10 +5,14 @@ using BEAST
 
 include("utils/showfn.jl")
 
-Tetrs = CompScienceMeshes.tetmeshsphere(1.0, 0.40)
+Tetrs = CompScienceMeshes.tetmeshsphere(1.0, 0.20)
 Faces = CompScienceMeshes.skeleton_fast(Tetrs, 2)
 Edges = CompScienceMeshes.skeleton_fast(Tetrs, 1)
 Nodes = CompScienceMeshes.skeleton_fast(Tetrs, 0)
+
+hemi = submesh(Tetrs) do Tetr
+    cartesian(CompScienceMeshes.center(chart(Tetrs, Tetr)))[3] < 0
+end
 
 bnd_Faces = boundary(Tetrs)
 bnd_Edges = CompScienceMeshes.skeleton_fast(bnd_Faces, 1)
@@ -22,65 +26,83 @@ int_Nodes = submesh(!in(bnd_Nodes), Nodes)
 @show length(int_Faces)
 
 primal1 = BEAST.nedelecc3d(Tetrs, int_Edges)
-primal2 = BEAST.nedelecd3d(Tetrs, int_Faces)
+primal2 = BEAST.nedelecd3d(Tetrs, Faces)
 
 Dir = bnd_Faces
 Neu = submesh(!in(Dir), bnd_Faces)
-dual1 = BEAST.dual1forms(Tetrs, int_Faces, Dir)
-dual2_old = BEAST.dual2forms(Tetrs, int_Edges, Neu)
+
+dual1 = BEAST.dual1forms(Tetrs, Faces, Dir)
 dual2 = BEAST.dual2forms_new(Tetrs, int_Edges, Dir)
 
-TF = connectivity(int_Faces, Tetrs)
-FE = connectivity(int_Edges, int_Faces)
-EN = connectivity(int_Nodes, int_Edges)
+@assert numfunctions(primal1) == numfunctions(dual2)
+@assert numfunctions(primal2) == numfunctions(dual1)
 
 Id = BEAST.Identity()
 G = assemble(Id, dual1, primal2)
 H = assemble(Id, dual2, primal1)
-A = assemble(Id, primal2, primal2)
-B = assemble(Id, curl(primal1), curl(primal1))
+
+TF = connectivity(Faces, Tetrs)
+FE = connectivity(int_Edges, Faces)
+EN = connectivity(int_Nodes, int_Edges)
+
 @show norm(G)
+@show norm(H)
 @show norm(TF*G*FE)
 @show norm(FE*H*EN)
 
-B2 = FE'*A*FE;
-@show norm(B - B2)
+# A = assemble(Id, primal2, primal2)
+# B = assemble(Id, curl(primal1), curl(primal1))
+# B2 = FE'*A*FE;
+# @show norm(B - B2)
 
-EV = eigen(B)
-idx = findfirst(abs.(EV.values) .> 1e-6)
-u = real(EV.vectors[:,idx+1])
+# EV = eigen(B)
+# idx = findfirst(abs.(EV.values) .> 1e-6)
+# u = real(EV.vectors[:,idx+1])
 
-hemi = submesh(Tetrs) do Tetr
-    cartesian(CompScienceMeshes.center(chart(Tetrs, Tetr)))[3] < 0
-end
-
-primal1_hemi = restrict(primal1, hemi)
-trace_primal1_hemi = BEAST.ttrace(primal1_hemi, boundary(hemi))
-fcr, geo = facecurrents(u, trace_primal1_hemi)
-Plotly.plot(patch(geo, norm.(fcr)))
+# primal1_hemi = restrict(primal1, hemi)
+# trace_primal1_hemi = BEAST.ttrace(primal1_hemi, boundary(hemi))
+# fcr, geo = facecurrents(u, trace_primal1_hemi)
+# Plotly.plot(patch(geo, norm.(fcr)))
 
 
-tetrs, bnd, dir, v2t, v2n = BEAST.dual1forms_init(Tetrs, Dir)
-duals11 = BEAST.dual1forms_body(int_Faces[collect(1:10)], tetrs, bnd, dir, v2t, v2n)
+# tetrs, bnd, dir, v2t, v2n = BEAST.dual1forms_init(Tetrs, Dir)
+# duals11 = BEAST.dual1forms_body(int_Faces[collect(1:10)], tetrs, bnd, dir, v2t, v2n)
 
-Ggf = assemble(Id, dual1, primal2);
+# Ggf = assemble(Id, dual1, primal2);
 Cgg = assemble(Id, dual1, curl(primal1))
 
-
-Cfg = Ggf \ Cgg;
-Conn = Matrix(connectivity(int_Edges, int_Faces))
-@show norm(Cfg - Conn)
+# Cfg = Ggf \ Cgg;
+# Conn = Matrix(connectivity(int_Edges, Faces))
+# @show norm(Cfg - Conn)
 
 Zpp = zeros(numfunctions(primal1), numfunctions(primal1))
 Zdd = zeros(numfunctions(dual1), numfunctions(dual1))
 
-Ipp = assemble(Id, primal1, primal1)
-Idd = assemble(Id, dual1, dual1)
+ϵ = BEAST.Multiplicative(p -> 1.0)
+μ = BEAST.Multiplicative(p -> 1.0)
+
+Ipp = assemble(ϵ, primal1, primal1)
+Idd = assemble(μ, dual1, dual1)
 
 Zpd = zeros(numfunctions(primal1), numfunctions(dual1))
 
 C = [Zpp Cgg'; -Cgg Zdd];
 J = [Ipp Zpd; Zpd' Idd];
+γ = 1.0 * im
 
-P = inv(2J - C) * (2J + C);
-Q = (2J - C) * inv(2J + C);
+# P = inv(2J - C) * (2J + C);
+# Q = (2J - C) * inv(2J + C);
+
+jg = assemble(BEAST.ScalarTrace(p -> point(1,0,0)), primal1)
+mG = zeros(numfunctions(dual1))
+
+b = [jg; mG]
+u = (C - γ*J) \ b
+
+eg = u[1:numfunctions(primal1)]
+hG = u[numfunctions(primal1)+1:end]
+
+primal1_hemi = restrict(primal1, hemi)
+trace_primal1_hemi = BEAST.ttrace(primal1_hemi, boundary(hemi))
+fcr, geo = facecurrents(eg, trace_primal1_hemi)
+Plotly.plot(patch(geo, norm.(fcr)))
