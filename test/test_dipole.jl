4563a6abff5fd3d962ce1189b5c21848485e3850
Simon B. Adrian <simon.adrian@ieee.org>
2021-05-07 20:37:30 +0200

    Test for dipole excitation
    The test is an example of using an dipole as a manufactured
    solution, i.e., we have an analytical solution for
    arbitrary geometries.

---
+++ test/test_dipole.jl
@@ -0,0 +1,106 @@
+using Test
+
+using CompScienceMeshes
+using BEAST
+using StaticArrays
+using LinearAlgebra
+
+c = 3e8
+μ = 4*π*1e-7
+ε = 1/(μ*c^2)
+f = 1e8
+λ = c/f
+k = 2*π/λ
+ω = k*c
+η = sqrt(μ/ε)
+
+a = 1
+Γ_orig = CompScienceMeshes.meshcuboid(a,a,a,0.3)
+Γ = translate(Γ_orig,SVector(-a/2,-a/2,-a/2))
+
+Φ, Θ = [0.0], range(0,stop=π,length=100)
+pts = [point(cos(ϕ)*sin(θ), sin(ϕ)*sin(θ), cos(θ)) for ϕ in Φ for θ in Θ]
+
+E = electricdipole(location=SVector(0,0,0), 
+                    orientation=1e-9.*SVector(0.5,0.5,0), 
+                    wavenumber=k, 
+                    permittivity=ε, 
+                    permeability=μ)
+
+n = BEAST.NormalVector()
+
+𝒆 = (n × E) × n
+H = (-1/(im*μ*ω))*curl(E)
+𝒉 = (n × H) × n
+
+𝓣 = Maxwell3D.singlelayer(wavenumber=k)
+𝓝 = BEAST.NCross()
+𝓚 = Maxwell3D.doublelayer(wavenumber=k)
+
+X = raviartthomas(Γ)
+Y = buffachristiansen(Γ)
+
+T = Matrix(assemble(𝓣,X,X))
+e = Vector(assemble(𝒆,X))
+j_EFIE = T\e
+
+nf_E_EFIE = potential(MWSingleLayerField3D(wavenumber=k), pts, j_EFIE, X)
+nf_H_EFIE = potential(BEAST.MWDoubleLayerField3D(wavenumber=k), pts, j_EFIE, X) ./ η
+ff_E_EFIE = potential(MWFarField3D(wavenumber=k), pts, j_EFIE, X)
+
+@test norm(nf_E_EFIE - E.(pts))/norm(E.(pts)) ≈ 0 atol=0.01
+@test norm(nf_H_EFIE - H.(pts))/norm(H.(pts)) ≈ 0 atol=0.01
+@test norm(ff_E_EFIE - E.(pts, isfarfield=true))/norm(E.(pts, isfarfield=true)) ≈ 0 atol=0.001
+
+K_bc = Matrix(assemble(𝓚,Y,X))
+G_nxbc_rt = Matrix(assemble(𝓝,Y,X))
+h_bc = η*Vector(assemble(𝒉,Y))
+M_bc = -0.5*G_nxbc_rt + K_bc
+j_BCMFIE = M_bc\h_bc
+
+nf_E_BCMFIE = potential(MWSingleLayerField3D(wavenumber=k), pts, j_BCMFIE, X)
+nf_H_BCMFIE = potential(BEAST.MWDoubleLayerField3D(wavenumber=k), pts, j_BCMFIE, X) ./ η
+ff_E_BCMFIE = potential(MWFarField3D(wavenumber=k), pts, j_BCMFIE, X)
+
+@test norm(nf_E_BCMFIE - E.(pts))/norm(E.(pts)) ≈ 0 atol=0.01
+@test norm(nf_H_BCMFIE - H.(pts))/norm(H.(pts)) ≈ 0 atol=0.01
+@test norm(ff_E_BCMFIE - E.(pts, isfarfield=true))/norm(E.(pts, isfarfield=true)) ≈ 0 atol=0.01
+
+E = magneticdipole(location=SVector(0,0,0), 
+                    orientation=1e-9.*SVector(0.5,0.5,0), 
+                    wavenumber=k, 
+                    permittivity=ε, 
+                    permeability=μ)
+
+𝒆 = (n × E) × n
+H = (-1/(im*μ*ω))*curl(E)
+𝒉 = (n × H) × n
+
+X = raviartthomas(Γ)
+Y = buffachristiansen(Γ)
+
+T = Matrix(assemble(𝓣,X,X))
+e = Vector(assemble(𝒆,X))
+j_EFIE = T\e
+
+nf_E_EFIE = potential(MWSingleLayerField3D(wavenumber=k), pts, j_EFIE, X)
+nf_H_EFIE = potential(BEAST.MWDoubleLayerField3D(wavenumber=k), pts, j_EFIE, X) ./ η
+ff_E_EFIE = potential(MWFarField3D(wavenumber=k), pts, j_EFIE, X)
+
+@test norm(nf_E_EFIE - E.(pts))/norm(E.(pts)) ≈ 0 atol=0.01
+@test norm(nf_H_EFIE - H.(pts))/norm(H.(pts)) ≈ 0 atol=0.01
+@test norm(ff_E_EFIE - E.(pts, isfarfield=true))/norm(E.(pts, isfarfield=true)) ≈ 0 atol=0.01
+
+K_bc = Matrix(assemble(𝓚,Y,X))
+G_nxbc_rt = Matrix(assemble(𝓝,Y,X))
+h_bc = η*Vector(assemble(𝒉,Y))
+M_bc = -0.5*G_nxbc_rt + K_bc
+j_BCMFIE = M_bc\h_bc
+
+nf_E_BCMFIE = potential(MWSingleLayerField3D(wavenumber=k), pts, j_BCMFIE, X)
+nf_H_BCMFIE = potential(BEAST.MWDoubleLayerField3D(wavenumber=k), pts, j_BCMFIE, X) ./ η
+ff_E_BCMFIE = potential(MWFarField3D(wavenumber=k), pts, j_BCMFIE, X)
+
+@test norm(nf_E_BCMFIE - E.(pts))/norm(E.(pts)) ≈ 0 atol=0.01
+@test norm(nf_H_BCMFIE - H.(pts))/norm(H.(pts)) ≈ 0 atol=0.01
+@test norm(ff_E_BCMFIE - E.(pts, isfarfield=true))/norm(E.(pts, isfarfield=true)) ≈ 0 atol=0.01
