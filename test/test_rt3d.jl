f28a7fbf429b72c46d3b0740ecd4d6ddb0eb7d9c
Kristof Cools <krcools@gmail.com>
2022-02-08 15:33:50 +0100

    gradient of Lag on triangles more efficient

--- test/test_rt3d.jl
+++ test/test_rt3d.jl
@@ -18,6 +18,28 @@ fcs = BEAST.faces(tet)
 @test dot(rs(nbd3)[3].value, normal(fcs[3])) > 0
 @test dot(rs(nbd4)[4].value, normal(fcs[4])) > 0
 
+dot(rs(nbd1)[1].value, normal(fcs[1])) * volume(fcs[1]) ≈ 1
+dot(rs(nbd2)[2].value, normal(fcs[2])) * volume(fcs[2]) ≈ 1
+dot(rs(nbd3)[3].value, normal(fcs[3])) * volume(fcs[2]) ≈ 1
+dot(rs(nbd4)[4].value, normal(fcs[4])) * volume(fcs[4]) ≈ 1
+
+@test dot(rs(nbd1)[2].value, normal(fcs[1])) * volume(fcs[1]) ≈ 0  atol=1e-8
+@test dot(rs(nbd1)[3].value, normal(fcs[1])) * volume(fcs[1]) ≈ 0  atol=1e-8
+@test dot(rs(nbd1)[4].value, normal(fcs[1])) * volume(fcs[1]) ≈ 0  atol=1e-8
+
+@test dot(rs(nbd2)[1].value, normal(fcs[2])) * volume(fcs[2]) ≈ 0  atol=1e-8
+@test dot(rs(nbd2)[3].value, normal(fcs[2])) * volume(fcs[2]) ≈ 0  atol=1e-8
+@test dot(rs(nbd2)[4].value, normal(fcs[2])) * volume(fcs[2]) ≈ 0  atol=1e-8
+
+@test dot(rs(nbd3)[1].value, normal(fcs[3])) * volume(fcs[3]) ≈ 0  atol=1e-8
+@test dot(rs(nbd3)[2].value, normal(fcs[3])) * volume(fcs[3]) ≈ 0  atol=1e-8
+@test dot(rs(nbd3)[4].value, normal(fcs[3])) * volume(fcs[3]) ≈ 0  atol=1e-8
+
+@test dot(rs(nbd4)[1].value, normal(fcs[4])) * volume(fcs[4]) ≈ 0  atol=1e-8
+@test dot(rs(nbd4)[2].value, normal(fcs[4])) * volume(fcs[4]) ≈ 0  atol=1e-8
+@test dot(rs(nbd4)[3].value, normal(fcs[4])) * volume(fcs[4]) ≈ 0  atol=1e-8
+
+
 ctr = cartesian(center(tet))
 @test dot(cartesian(nbd1)-ctr, normal(fcs[1])) > 0
 @test dot(cartesian(nbd2)-ctr, normal(fcs[2])) > 0
