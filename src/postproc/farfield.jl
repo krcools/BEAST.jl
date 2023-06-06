ab2dc497f781e064e5a798feaa6875622084ef1b
Kristof Cools <krcools@gmail.com>
2023-06-06 09:12:20 +0000

    itsolvers allows passing both abstol and reltol

--- src/postproc/farfield.jl
+++ src/postproc/farfield.jl
@@ -1,6 +1,7 @@
 abstract type FarField end
 
 defaultquadstrat(op::FarField, basis) = SingleNumQStrat(3)
+defaultquadstrat(op::FarField, basis::DirectProductSpace) = defaultquadstrat(op, basis.factors[1])
 
 quaddata(op::FarField,rs,els,qs::SingleNumQStrat) = quadpoints(rs,els,(qs.quad_rule,))
 quadrule(op::FarField,refspace,p,y,q,el,qdata,qs::SingleNumQStrat) = qdata[1,q]
