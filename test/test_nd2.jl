3b70b4c44b670114b4f553fa866abc67b5d956f3
jay prakash <jayprakashdav20@gmail.com>
2024-03-12 18:43:14 +0000

    bug fix test file

--- test/test_nd2.jl
+++ test/test_nd2.jl
@@ -4,13 +4,13 @@ using BEAST
 using Test
 
 
-"""
+#= """
 This test is achieved by projecting the coefficients of 1st degree 
 curl conforming basis functions onto the 2nd degree basis functions
 and then projecting back the obtained coefficients of the 2nd degree
 basis functions onto the 1st degree basis functions. The resultant
 operator should be an Identity operator.
-"""
+""" =#
 
 local m = meshrectangle(1.0,1.0,0.5)
 tol = 1e-10
--- test/test_rt2.jl
+++ test/test_rt2.jl
@@ -3,13 +3,14 @@ using BEAST
 
 using Test
 
-"""
+#= """
 This test is achieved by projecting the coefficients of 1st degree 
 div conforming basis functions onto the 2nd degree basis functions
 and then projecting back the obtained coefficients of the 2nd degree
 basis functions onto the 1st degree basis functions. The resultant
 operator should be an Identity operator.
-"""
+""" =#
+
 local m = meshrectangle(1.0,1.0,0.5)
 tol = 1e-10
 X2 = BEAST.raviartthomas2(m)
