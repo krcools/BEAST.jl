196e5b3913bbcbc59772e0f604a0bf7b8864993f
jayprakashdav20 <jayprakashdav20@gmail.com>
2024-10-30 17:44:12 +0100

    test for non conforming assembly

---
+++ test/test_nonconforming_assembly.jl
@@ -0,0 +1,23 @@
+using BEAST, CompScienceMeshes, LinearAlgebra, Test
+
+#= 
+    This script test the strategy for the assembly of an integral operator
+    for basis and test function associated with nonconforimg mesh
+    It is tested by comparing the elements of interaction matrix obtained 
+    using a given basis and test functions with the elements of interaction matrix obtained
+    when the function are are swapped. 
+=#
+tol = 1e-2
+
+m1 = meshcuboid(1.0,1.0,0.25,0.2)
+m2 = meshcuboid(1.0,1.0,0.25,0.15)
+
+X1 = BEAST.raviartthomas(m1)
+X2 = BEAST.raviartthomas(m2)
+t = Maxwell3D.singlelayer(wavenumber=1.0)
+
+parent_qstrat = BEAST.DoubleNumSauterQstrat(7,8,8,8,8,8)
+qstrat = BEAST.NonConformingIntegralOpQStrat(parent_qstrat)
+Z21 = BEAST.assemble(t, X2, X1, quadstrat=qstrat)
+Z12 = BEAST.assemble(t, X1, X2, quadstrat=qstrat)
+@test maximum(norm.(Z21-transpose(Z12)))<tol
