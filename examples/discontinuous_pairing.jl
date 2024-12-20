38e0207c402131ed1cd6ba6dfbbc1bc813258c4e
Kristof Cools <krcools@gmail.com>
2024-12-20 10:41:27 +0100

    discontinuous_pairing example

--- examples/discontinuous_pairing.jl
+++ examples/discontinuous_pairing.jl
@@ -39,3 +39,43 @@ using Plots
 plot(svdA.S)
 plot!(svdB.S)
 plot!(svdC.S)
+
+E = Maxwell3D.planewave(
+    direction=point(0,0,1),
+    polarization=point(1,0,0),
+    wavenumber=1.0)
+
+e = (n × E) × n
+
+struct SingField{T} <: BEAST.Functional
+    a::T
+    b::T
+end
+
+function (f::SingField)(p)
+    x = cartesian(p)
+    x[3] ≈ 1.0 || return point(0,0,0)
+    return sqrt(f.b - x[2]) * sqrt(x[2] - f.a) / sqrt(f.b-x[1]) / sqrt(x[1]-f.a) * point(0,1,0)
+end
+
+function BEAST.integrand(f::SingField, gx, ϕx)
+    return dot(gx[1], ϕx)
+end
+
+e = SingField(0.0, 1.0)
+
+b = assemble(e, X)
+G = assemble(id, X, X)
+u = G \ b
+
+import Plotly
+fcr, geo = facecurrents(u, X)
+Plotly.plot(
+    [
+        # patch(geo, norm.(fcr), opacity=0.5),
+        CompScienceMeshes.cones(geo, real.(fcr), sizeref=10.0),
+    ])
+
+q = adjoint(svdA.U) * HY * b
+plot(log10.(abs.(q)))
+plot!(log10.(abs.(svdA.S)))
