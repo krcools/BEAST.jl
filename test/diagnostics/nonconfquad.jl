55ec0a3b8cc98ff7b60cef65eed15c91e531e1ac
Kristof Cools <krcools@gmail.com>
2026-01-10 16:07:31 +0100

    added diagnostics for mutually non-conforming meshes

--- test/diagnostics/nonconfquad.jl
+++ test/diagnostics/nonconfquad.jl
@@ -139,7 +139,7 @@ end
 
 
 
-@testitem "Non-conf mesh - vectorial" tags=[:diagnostics] begin
+@testitem "Non-conf mesh: cuboid, vectorial, regular" tags=[:diagnostics] begin
     using CompScienceMeshes
 
     function CompScienceMeshes.volume(m::CompScienceMeshes.AbstractMesh)
@@ -165,11 +165,31 @@ end
     BEAST.scalartype(::typeof(E1)) = Float64
     BEAST.scalartype(::typeof(E2)) = Float64
 
-    h1 = 0.2 * 0.75^3
-    h2 = 0.2 * 0.75^4
+    h1 = 0.2 * 0.75^6
+    h2 = 0.2 * 0.75^8
 
-    Γ1 = meshcuboid(1.0, 1.0, 1.0, h1; generator=:gmsh)
-    Γ2 = meshcuboid(1.0, 1.0, 1.0, h2; generator=:gmsh)
+    function meshbox(;h)
+        geofile = pkgdir(BEAST) * "/test/assets/twoboxes.geo"
+        G10 = meshgeo(geofile; physical="G10", h=h)
+        G20 = meshgeo(geofile; physical="G20", h=h)
+        G12 = meshgeo(geofile; physical="G12", h=h)
+
+        G20.vertices = G10.vertices
+        G12.vertices = G10.vertices
+
+        G0, S1 = CompScienceMeshes.union([-G10, -G20], Val{:submeshes})
+        G1, S2 = CompScienceMeshes.union([G10, G12], Val{:submeshes})
+        G2, S3 = CompScienceMeshes.union([G20, -G12], Val{:submeshes})
+        ∂Ω = [G0,G1,G2]
+        Γ = [S1,S2,S3]
+        return ∂Ω[3]
+    end
+
+    # Γ1 = meshcuboid(1.0, 1.0, 1.0, h1; generator=:gmsh)
+    # Γ2 = meshcuboid(1.0, 1.0, 1.0, h2; generator=:gmsh)
+
+    Γ1 = meshbox(h=h1)
+    Γ2 = meshbox(h=h2)
 
     pred = (m,c) -> begin
         x = chart(m,c) |> center |> cartesian
@@ -208,6 +228,99 @@ end
 
     @test abs(r12 - r11) < 1e-10
     @test abs(r12 - r22) < 1e-10 
+end
+
+
+@testitem "Non-conf mesh: cuboid, vectorial, local" tags=[:diagnostics] begin
+    using CompScienceMeshes
+
+    function CompScienceMeshes.volume(m::CompScienceMeshes.AbstractMesh)
+        sum(CompScienceMeshes.volume(chart(m,c)) for c in m)
+    end
+
+    # struct ConstantIntegralOp{T} <: BEAST.IntegralOperator
+    #     constant::T
+    # end
+
+    # BEAST.scalartype(op::ConstantIntegralOp{T}) where {T} = T
+
+    # function (igd::BEAST.Integrand{<:ConstantIntegralOp})(p,q,f,g)
+    #     c = igd.operator.constant
+
+    #     BEAST._integrands(f,g) do fi,gj
+    #         dot(fi.value, gj.value) * c
+    #     end
+    # end
+
+    E1(x) = point(1.0, 1.0, 0.0)
+    E2(x) = point(0.0, 1.0, 1.0)
+    BEAST.scalartype(::typeof(E1)) = Float64
+    BEAST.scalartype(::typeof(E2)) = Float64
+
+    h1 = 0.2 * 0.75^6
+    h2 = 0.2 * 0.75^8
+
+    function meshbox(;h)
+        geofile = pkgdir(BEAST) * "/test/assets/twoboxes.geo"
+        G10 = meshgeo(geofile; physical="G10", h=h)
+        G20 = meshgeo(geofile; physical="G20", h=h)
+        G12 = meshgeo(geofile; physical="G12", h=h)
+
+        G20.vertices = G10.vertices
+        G12.vertices = G10.vertices
+
+        G0, S1 = CompScienceMeshes.union([-G10, -G20], Val{:submeshes})
+        G1, S2 = CompScienceMeshes.union([G10, G12], Val{:submeshes})
+        G2, S3 = CompScienceMeshes.union([G20, -G12], Val{:submeshes})
+        ∂Ω = [G0,G1,G2]
+        Γ = [S1,S2,S3]
+        return ∂Ω[3]
+    end
+
+    # Γ1 = meshcuboid(1.0, 1.0, 1.0, h1; generator=:gmsh)
+    # Γ2 = meshcuboid(1.0, 1.0, 1.0, h2; generator=:gmsh)
+
+    Γ1 = meshbox(h=h1)
+    Γ2 = meshbox(h=h2)
+
+    pred = (m,c) -> begin
+        x = chart(m,c) |> center |> cartesian
+        !(x[3] ≈ 0.0)
+    end
+    Γ1 = submesh(pred, Γ1)
+    Γ2 = submesh(pred, Γ2)
+
+    v1 = CompScienceMeshes.volume(Γ1)
+    v2 = CompScienceMeshes.volume(Γ2)
+
+    X1 = raviartthomas(Γ1, skeleton(Γ1,1))
+    X2 = raviartthomas(Γ2, skeleton(Γ2,1))
+
+    Id = BEAST.Identity()
+    I11 = assemble(Id, X1, X1)
+    I22 = assemble(Id, X2, X2)
+
+    u11 = I11 \ assemble(n × E1, X1)
+    u12 = I22 \ assemble(n × E1, X2)
+    u21 = I11 \ assemble(n × E2, X1)
+    u22 = I22 \ assemble(n × E2, X2)
+
+    # op = ConstantIntegralOp(1.0)
+    op = Identity()
+
+    # qs1 = BEAST.DoubleNumQStrat(3,3)
+    # qs2 = BEAST.NonConformingIntegralOpQStrat(qs1)
+
+    A12 = assemble(op, X1, X2)
+    A11 = assemble(op, X1, X1)
+    A22 = assemble(op, X2, X2)
+
+    @show r12 = dot(u11, A12*u22)
+    @show r11 = dot(u11, A11*u21)
+    @show r22 = dot(u12, A22*u22)
+
+    @test abs(r12 - r11) < 1e-5
+    @test abs(r12 - r22) < 1e-5 
 end
 
 
